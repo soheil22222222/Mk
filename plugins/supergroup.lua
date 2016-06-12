bb537c038443507b149e7ebf9fdba767e4d0b222
sargardan <soheil22222222@users.noreply.github.com>
2016-06-12 14:29:56 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1319,7 +1319,7 @@ local function run(msg, matches)
 				return "Create a link using /newlink first!\n\nOr if I am not creator use /setlink to set your link"
 			end
 			savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
-			return "Group link:\n"..group_link
+			return "Namegroup:["..string.gsub(msg.to.print_name, "_", " ").."]\nGroup link:\n"..group_link
 		end
 
 		if matches[1] == "invite" and is_sudo(msg) then
